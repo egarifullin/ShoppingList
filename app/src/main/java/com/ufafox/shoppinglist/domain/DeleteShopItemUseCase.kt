86080120405eebc5b9f72774f29907dda9862fe6
eugen <eugeny.garifullin@gmail.com>
2022-03-28 16:59:38 +0300

    Initial commit

---
+++ app/src/main/java/com/ufafox/shoppinglist/domain/DeleteShopItemUseCase.kt
@@ -0,0 +1,7 @@
+package com.ufafox.shoppinglist.domain
+
+class DeleteShopItemUseCase(private val shopListRepository: ShopListRepository) {
+    fun deleteShopItem(shopItem: ShopItem){
+        shopListRepository.deleteShopItem(shopItem)
+    }
+}
---
+++ app/src/main/java/com/ufafox/shoppinglist/domain/GetShopListUseCase.kt
@@ -0,0 +1,8 @@
+package com.ufafox.shoppinglist.domain
+
+class GetShopListUseCase (private val shopListRepository: ShopListRepository) {
+
+    fun getShopList():List<ShopItem>{
+        return shopListRepository.getShopList()
+    }
+}
