a669ce3c81455e33d797f55ce7e256561d4712d2
eugen <eugeny.garifullin@gmail.com>
2022-04-05 15:56:04 +0300

    editing fragment's arguments

--- app/src/main/java/com/ufafox/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/ufafox/shoppinglist/presentation/MainActivity.kt
@@ -27,7 +27,7 @@ class MainActivity : AppCompatActivity() {
 
         val buttonAddItem = findViewById<FloatingActionButton>(R.id.button_add_shop_item)
         buttonAddItem.setOnClickListener {
-            val intent = ShopItemFragment.newIntentAddItem(this)
+            val intent = ShopItemActivity.newIntentAddItem(this)
             startActivity(intent)
         }
     }
